f39b9b466895e31fbc68da88b71b0877c3cae49a
Aaditya Bansal <aadityabansal522@gmail.com>
2026-01-23 10:13:21 +0530

    Fixed Funtionality Bugs

--- server.js
+++ server.js
@@ -40,7 +40,7 @@ app.use("/uploads", express.static(path.join(process.cwd(), "uploads")));
 
 /* DB */
 const PORT = process.env.PORT || 5000;
-const MONGO_URI = "mongodb://localhost:27017/econ";
+const MONGO_URI = 'mongodb+srv://Aaditya522:aaditya123@cluster0.nprlzh9.mongodb.net/';
 
 const startServer = async () => {
   try {
@@ -52,7 +52,7 @@ const startServer = async () => {
     app.use(
       session({
         name: "nearmart.sid",
-        secret: process.env.SESSION_SECRET,
+        secret: "Mock_secret_key",
         resave: false,
         saveUninitialized: false,
         store: MongoStore.create({
@@ -85,3 +85,85 @@ const startServer = async () => {
 };
 
 startServer();
+
+
+
+
+// import dotenv from "dotenv";
+// dotenv.config(); 
+
+// import express from "express";
+// import path from "path";
+// import session from "express-session";
+// import MongoStore from "connect-mongo";
+// import cors from "cors";
+// import mongoose from "mongoose";
+
+// // Routes
+// import authRoutes from "./routes/authRoutes.js";
+// import productRoutes from "./routes/productRoutes.js";
+// import cartRoutes from "./routes/cartRoutes.js";
+// import adminRoutes from "./routes/adminRoutes.js";
+// import retailerRoutes from "./routes/retailerRoutes.js";
+// import orderRoutes from "./routes/orderRoutes.js";
+
+// const app = express();
+
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+
+
+// app.use(cors({
+//   origin: "http://localhost:3000",
+//   credentials: true
+// }));
+
+// app.use("/uploads", express.static(path.join(process.cwd(), "uploads")));
+
+// const MONGO_URI = "mongodb://127.0.0.1:27017/econ";
+
+// const startServer = async () => {
+//   try {
+//     await mongoose.connect(MONGO_URI);
+//     console.log("MongoDB connected");
+
+//     const SESSION_HOURS = 8;
+
+//     app.use(session({
+//       name: "nearmart.sid",
+//       secret: "MY_SECRET_KEY",
+//       resave: false,
+//       saveUninitialized: false,
+//       store: MongoStore.create({
+//         mongoUrl: MONGO_URI,
+//         collectionName: "sessions",
+//         ttl: 60 * 60 * SESSION_HOURS
+//       }),
+//       cookie: {
+//         httpOnly: true,
+//         secure: false,
+//         sameSite: "lax",
+//         maxAge: 1000 * 60 * 60 * SESSION_HOURS
+//       }
+//     }));
+
+
+//     app.use("/", authRoutes);
+//     app.use("/", productRoutes);
+//     app.use("/", cartRoutes);
+//     app.use("/", orderRoutes);
+//     app.use("/", adminRoutes);
+//     app.use("/", retailerRoutes);
+
+
+//     app.listen(5000, () => {
+//       console.log("Server running on http://localhost:5000");
+//     });
+
+//   } catch (err) {
+//     console.error(" MongoDB connection failed:", err);
+//     process.exit(1);
+//   }
+// };
+
+// startServer();
